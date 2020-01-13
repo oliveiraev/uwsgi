3fadb13323f92326c470ffadaf480a31fa6eec92
Restyled.io <commits@restyled.io>
2020-01-13 15:09:52 +0000

    Restyled by black

--- tests/testrpc.py
+++ tests/testrpc.py
@@ -7,9 +7,9 @@ def hello():
 
 def application(env, start_response):
     try:
-        uwsgi.register_rpc("A"*300, hello)
-        start_response('500 Buffer Overflow', [('Content-Type', 'text/plain')])
+        uwsgi.register_rpc("A" * 300, hello)
+        start_response("500 Buffer Overflow", [("Content-Type", "text/plain")])
     except ValueError:
-        start_response('200 OK', [('Content-Type', 'text/plain')])
+        start_response("200 OK", [("Content-Type", "text/plain")])
 
     return ()

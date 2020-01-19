79203b6bd59a2e63aec65e340fe6e5a39a65fc3d
Restyled.io <commits@restyled.io>
2020-01-19 14:09:54 +0000

    Restyled by black

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from setuptools.command.build_ext import build_ext
 
 try:
     from wheel.bdist_wheel import bdist_wheel
+
     HAS_WHEEL = True
 except ImportError:
     HAS_WHEEL = False
@@ -27,16 +28,17 @@ def get_profile():
     is_pypy = False
     try:
         import __pypy__  # NOQA
+
         is_pypy = True
     except ImportError:
         pass
     if is_pypy:
-        profile = os.environ.get('UWSGI_PROFILE', 'buildconf/pypy.ini')
+        profile = os.environ.get("UWSGI_PROFILE", "buildconf/pypy.ini")
     else:
-        profile = os.environ.get('UWSGI_PROFILE', 'buildconf/default.ini')
-    if not profile.endswith('.ini'):
+        profile = os.environ.get("UWSGI_PROFILE", "buildconf/default.ini")
+    if not profile.endswith(".ini"):
         profile = "%s.ini" % profile
-    if '/' not in profile:
+    if "/" not in profile:
         profile = "buildconf/%s" % profile
 
     return profile
@@ -44,21 +46,21 @@ def get_profile():
 
 def patch_bin_path(cmd, conf):
 
-    bin_name = conf.get('bin_name')
+    bin_name = conf.get("bin_name")
 
     if not os.path.isabs(bin_name):
         print('Patching "bin_name" to properly install_scripts dir')
         try:
             if not os.path.exists(cmd.install_scripts):
                 os.makedirs(cmd.install_scripts)
-            conf.set('bin_name',
-                     os.path.join(cmd.install_scripts, conf.get('bin_name')))
+            conf.set(
+                "bin_name", os.path.join(cmd.install_scripts, conf.get("bin_name"))
+            )
         except Exception:
-            conf.set('bin_name', sys.prefix + '/bin/' + bin_name)
+            conf.set("bin_name", sys.prefix + "/bin/" + bin_name)
 
 
 class uWSGIBuilder(build_ext):
-
     def run(self):
         global uwsgi_compiled
         if not uwsgi_compiled:
@@ -69,7 +71,6 @@ class uWSGIBuilder(build_ext):
 
 
 class uWSGIInstall(install):
-
     def run(self):
         global uwsgi_compiled
         if not uwsgi_compiled:
@@ -81,7 +82,6 @@ class uWSGIInstall(install):
 
 
 class uWSGIInstallLib(install_lib):
-
     def run(self):
         global uwsgi_compiled
         if not uwsgi_compiled:
@@ -93,6 +93,7 @@ class uWSGIInstallLib(install_lib):
 
 
 if HAS_WHEEL:
+
     class uWSGIWheel(bdist_wheel):
         def finalize_options(self):
             bdist_wheel.finalize_options(self)
@@ -100,14 +101,13 @@ if HAS_WHEEL:
 
 
 class uWSGIDistribution(Distribution):
-
     def __init__(self, *attrs):
         Distribution.__init__(self, *attrs)
-        self.cmdclass['install'] = uWSGIInstall
-        self.cmdclass['install_lib'] = uWSGIInstallLib
-        self.cmdclass['build_ext'] = uWSGIBuilder
+        self.cmdclass["install"] = uWSGIInstall
+        self.cmdclass["install_lib"] = uWSGIInstallLib
+        self.cmdclass["build_ext"] = uWSGIBuilder
         if HAS_WHEEL:
-            self.cmdclass['bdist_wheel'] = uWSGIWheel
+            self.cmdclass["bdist_wheel"] = uWSGIWheel
 
     def is_pure(self):
         return False
@@ -116,37 +116,37 @@ class uWSGIDistribution(Distribution):
 def get_extra_require():
     req = []
     conf = uc.uConf(get_profile())
-    plugins = conf.get('main_plugin')
+    plugins = conf.get("main_plugin")
     if plugins:
-        plugins = plugins.split(',')
-        if 'greenlet' in plugins:
-            req.append('greenlet')
+        plugins = plugins.split(",")
+        if "greenlet" in plugins:
+            req.append("greenlet")
 
     return req
 
 
 setup(
-    name='uWSGI',
+    name="uWSGI",
     version=uc.uwsgi_version,
-    description='The uWSGI server',
-    author='Unbit',
-    author_email='info@unbit.it',
-    license='GPLv2+',
-    py_modules=['uwsgidecorators'],
+    description="The uWSGI server",
+    author="Unbit",
+    author_email="info@unbit.it",
+    license="GPLv2+",
+    py_modules=["uwsgidecorators"],
     distclass=uWSGIDistribution,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    install_requires=get_extra_require()
+    install_requires=get_extra_require(),
 )
